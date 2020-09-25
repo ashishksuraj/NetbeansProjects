d59958cc51d14969092e1992b127e2b39f8b98d3
ashishksuraj <ashishksuraj@gmail.com>
2020-09-25 15:01:59 +0530

    added first time all the files

---
+++ test/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        /*String[] hello1 = {"aaadeehiklstt","howar"};
+        String[] hello ={"latesthaiekda", "hello", "howar", "lol"};
+        char[] temp = hello[0].toCharArray();
+        Arrays.sort(temp);
+        String str = new String(temp);
+        hello[0] = str;
+        System.out.println(Arrays.toString(hello));
+        if (hello[2].contentEquals(hello1[1])) {
+            System.out.println("matched");
+        }
+        */
+        List<Integer> a = new List<Integer>;
+    }
+}
