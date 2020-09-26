d7c877d9975e73d40a2bce9364ec66a1b42fa9b5
ashishksuraj <ashishksuraj@gmail.com>
2020-09-26 12:01:06 +0530

    1st commit

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -2,20 +2,25 @@ package test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Test {
+
     public static void main(String[] args) {
-        /*String[] hello1 = {"aaadeehiklstt","howar"};
-        String[] hello ={"latesthaiekda", "hello", "howar", "lol"};
-        char[] temp = hello[0].toCharArray();
-        Arrays.sort(temp);
-        String str = new String(temp);
-        hello[0] = str;
-        System.out.println(Arrays.toString(hello));
-        if (hello[2].contentEquals(hello1[1])) {
-            System.out.println("matched");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
         }
-        */
-        List<Integer> a = new List<Integer>;
+        Arrays.sort(a);
+        int h=0;
+        for (int i = n-1; i > 0; i--) {
+            if (a[i]==a[i-1]) {
+                h++;
+            }
+            else break;
+        }
+        System.out.println(h+1);
     }
 }
