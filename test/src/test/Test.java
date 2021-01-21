b1a455962b95ba0224a9ac7e6d136623910f551e
ashishksuraj <ashishksuraj@gmail.com>
2021-01-21 21:49:16 +0530

    added new files

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -7,20 +7,7 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scan.nextInt();
-        }
-        Arrays.sort(a);
-        int h=0;
-        for (int i = n-1; i > 0; i--) {
-            if (a[i]==a[i-1]) {
-                h++;
-            }
-            else break;
-        }
-        System.out.println(h+1);
+        String ss = "ss";
+        
     }
 }
